e8ab733e83b6caaf4c5e5d48c915c594385d2ab3
Robothong <wanghong@bme.pumc.edu.cn>
2021-02-10 13:22:02 +0800

    test

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-<<<<<<< HEAD
 #include "vtkCommand.h"
-=======
->>>>>>> a70a6c00387d8337b4038372312d3f74ff6b549c
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
